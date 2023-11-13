a42c3afb8d100db396f313cdfcc95e5e2f4e6a34
Kacper <kacper@uwm.edu.pl>
2023-11-13 11:58:21 +0100

    edycja lab 05

--- lab05/lab05_zad2.sql
+++ lab05/lab05_zad2.sql
@@ -1,1 +1,14 @@
-alter table postac add column pesel varchar(11) primary key not null;
+# pkt a
+alter table postac add column pesel char(11) first;
+update postac set pesel='12345678910' where id_postaci=1;
+select * from postac;
+update postac set pesel='12345678910'+ id_postaci;
+alter table postac add primary key(pesel); 
+
+# pkt b
+
+alter table postac modify rodzaj enum('wiking','ptak','kobieta','syrena');
+
+# pkt c
+
+insert into postac values('12345678917',default,'Gertruda Nieszczera','syrena',2000-10-10,default,default);
