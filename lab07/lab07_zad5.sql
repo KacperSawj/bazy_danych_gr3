435c259808c81c4e3809ce8b5ac17d9b8f047a6c
Kacper <kacper@uwm.edu.pl>
2023-12-11 10:59:40 +0100

    dokończenie zad5

--- lab07/lab07_zad5.sql
+++ lab07/lab07_zad5.sql
@@ -7,6 +7,17 @@ where k.rodzaj not in ('malpa','waz') and e.ilosc<30
 group by k.rodzaj;
 
 # pkt 2
-select p.rodzaj, p.min, p.max from
-(select rodzaj, min(dataUr) min, max(dataUr) max from kreatura group by rodzaj) as p
-group by p.rodzaj;
+select a.nazwa, a.rodzaj, a.dataUr from kreatura a,
+(select rodzaj, min(dataUr) min, max(dataUr) max from kreatura group by rodzaj) b
+where b.min = a.dataUr or b.max = a.dataUr;
+
+# pkt 2 z union
+select 'najmlodsza', a.maxData, b.nazwa, a.rodzaj
+from (select max(dataUr) maxData, rodzaj from kreatura group by rodzaj) a,
+(select nazwa, dataUr from kreatura) b
+where a.maxData = b.dataUr
+union
+select 'najstarsza', a.minData, b.nazwa, a.rodzaj
+from (select min(dataUr) minData, rodzaj from kreatura group by rodzaj) a,
+(select nazwa, dataUr from kreatura) b
+where a.minData = b.dataUr;
