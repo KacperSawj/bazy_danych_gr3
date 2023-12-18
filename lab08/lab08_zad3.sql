7c69556cf0720a2d2b51607fb0e27f284f1e8208
Kacper <kacper@uwm.edu.pl>
2023-12-18 10:20:25 +0100

    dokończenie zad3, stworzenie pliku zad4

--- lab08/lab08_zad3.sql
+++ lab08/lab08_zad3.sql
@@ -5,7 +5,7 @@ left join sektor s on ew.sektor=s.id_sektora
 group by s.nazwa;
 
 # pkt 2
-select k.nazwa, if(count(u.id_uczestnika) > 0,'brał udział w wyprawie') from kreatura k
+select k.nazwa, if(count(u.id_uczestnika) > 0,'brał udział w wyprawie','nie brał udział w wyprawie') from kreatura k
 left join uczestnicy u on k.idKreatury=u.id_uczestnika
 left join wyprawa w on w.id_wyprawy=u.id_wyprawy
 group by k.nazwa;
